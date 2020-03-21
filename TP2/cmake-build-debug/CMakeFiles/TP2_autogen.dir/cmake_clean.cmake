7a029df4f1d66707342db95ac69321f26749736e
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-03-21 21:47:11 +0100

    Cir2 isen tp Qt

---
+++ TP2/cmake-build-debug/CMakeFiles/TP2_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "TP2_autogen"
+  "CMakeFiles/TP2_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/TP2_autogen"
+  "TP2_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TP2_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
