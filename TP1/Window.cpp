ffa13152a2cabe216932ce0f5b8d4083b260119c
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-03-21 21:39:04 +0100

    Tp1

---
+++ TP1/Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Théo on 3/21/2020.
+//
+
+#include "Window.h"
---
+++ TP1/Window.h
@@ -0,0 +1,14 @@
+//
+// Created by Théo on 3/21/2020.
+//
+
+#ifndef TP1_WINDOW_H
+#define TP1_WINDOW_H
+
+
+class Window {
+
+};
+
+
+#endif //TP1_WINDOW_H
